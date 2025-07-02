756ea4cff8dd98e90167f681f8426fd8a45dbcf3
sungrazer <jonwmeyer@gmail.com>
2025-07-02 15:20:33 -0600

    .

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-    echo "START: Building Alterx"
+    echo "START: Building uncover"
     apt update
     apt install -y build-essential ca-certificates wget unzip git
     apt install -y python3 python3-pip python-is-python3
@@ -9,7 +9,8 @@
     export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
     export GO111MODULE=on
     export CGO_ENABLED=1
+    export SHODAN_API_KEY=1WSR9UcKwjrD2ReQtGhVrGbFqVtyVnL4
     mkdir -p /go/src
     mkdir -p /go/bin
     go install -v github.com/projectdiscovery/uncover/cmd/uncover@latest
-    echo "END: Building Alterx"
+    echo "END: Building uncover"
